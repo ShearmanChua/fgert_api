209cf3c0534426ef47a2da0db16b18734cc8cc59
shearman <shearman@pop-os.localdomain>
2022-07-13 09:52:55 +0800

    new

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # fget_api
+
+api data format:
+
+dataframe with doc_id, text, text_tokens and entities
+
+Example:
+
+doc_id: 5603098-4
+
+Text: Alyan Muhammad Ali al - Wa'eli ( Arabic : ) ( born in 1970 in Yemen ) became wanted in 2002 , by the United States Department of Justice 's FBI , which was then seeking information about his identity and whereabouts .
+
+text_tokens: ['Alyan', 'Muhammad', 'Ali', 'al', '-', ""Wa'eli"", '(', 'Arabic', ':', ')', '(', 'born', 'in', '1970', 'in', 'Yemen', ')', 'became', 'wanted', 'in', '2002', ',', 'by', 'the', 'United', 'States', 'Department', 'of', 'Justice', ""'s"", 'FBI', ',', 'which', 'was', 'then', 'seeking', 'information', 'about', 'his', 'identity', 'and', 'whereabouts', '.']
+
+entities: [{'labels': ['/person'], 'start': 0, 'end': 6, 'mention': ""Alyan Muhammad Ali al - Wa'eli"", 'mention_id': '5603098-0'}, {'labels': ['/language'], 'start': 7, 'end': 8, 'mention': 'Arabic', 'mention_id': '5603098-1'}, {'labels': ['/location', '/location/country'], 'start': 15, 'end': 16, 'mention': 'Yemen', 'mention_id': '5603098-2'}, {'labels': ['/organization', '/organization/government_agency'], 'start': 24, 'end': 29, 'mention': 'United States Department of Justice', 'mention_id': '5603098-3'}, {'labels': ['/organization', '/organization/government_agency'], 'start': 30, 'end': 31, 'mention': 'FBI', 'mention_id': '5603098-4'}]
+
+* labels can be left as an empty string ''
--- test_request.py
+++ test_request.py
@@ -23,54 +23,6 @@ def predict_fget(dataset: Dict):
     return df
 
 
-def generate_entity_linking_df(results_df):
-
-    entities_linking_df = pd.DataFrame(columns=['doc_id','mention', 'mention_type','context_left','context_right'])
-
-    for idx, row in results_df.iterrows():
-        doc_id = row['doc_id']
-        if type(row['relations']) == str:
-            relations = ast.literal_eval(row['relations'])
-            tokens = ast.literal_eval(row['tokens'])
-        else:
-            relations =  row['relations']
-            tokens = row['tokens']
-        entities = []
-        for relation in relations:
-            head_entity = " ".join(tokens[relation['head_span'][0]:relation['head_span'][1]])
-            if head_entity not in entities:
-                print("Head Entity:")
-                print(head_entity)
-                left_context = " ".join(tokens[relation['head_span'][0]-100:relation['head_span'][0]])
-                print("Left context: ",left_context)
-                print("\n")
-                right_context = " ".join(tokens[relation['head_span'][1]:relation['head_span'][1]+100])
-                print("Right context: ",right_context)
-                print("\n")
-                entities_linking_df.loc[-1] = [doc_id, head_entity, relation['head_type'], left_context, right_context]  # adding a row
-                entities_linking_df.index = entities_linking_df.index + 1  # shifting index
-                entities_linking_df = entities_linking_df.sort_index()  # sorting by index
-                entities.append(head_entity)
-
-            tail_entity = " ".join(tokens[relation['tail_span'][0]:relation['tail_span'][1]])
-            if tail_entity not in entities:
-                print("Tail Entity:")
-                print(tail_entity)
-                left_context = " ".join(tokens[relation['tail_span'][0]-100:relation['tail_span'][0]])
-                print("Left context: ",left_context)
-                print("\n")
-                right_context = " ".join(tokens[relation['tail_span'][1]:relation['tail_span'][1]+100])
-                print("Right context: ",right_context)
-                print("\n")
-                entities_linking_df.loc[-1] = [doc_id, tail_entity, relation['tail_type'], left_context, right_context]  # adding a row
-                entities_linking_df.index = entities_linking_df.index + 1  # shifting index
-                entities_linking_df = entities_linking_df.sort_index()  # sorting by index
-                entities.append(tail_entity)
-
-    print(entities_linking_df.head())
-    return entities_linking_df
-
-
 if __name__ == '__main__':
 
     df = pd.read_csv("data/test.csv")
